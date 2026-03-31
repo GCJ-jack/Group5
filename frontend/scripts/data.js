4e60e19cef371a13d81faaedecf25e7c53fd1bdb
chaojun guo <3218818818005g@gmail.com>
2026-03-31 14:04:22 +0800

    display the new list in front end and make the login form simpler

--- frontend/scripts/data.js
+++ frontend/scripts/data.js
@@ -49,3 +49,45 @@ window.dashboardData = {
     "Dividend alert: AAPL payout in 3 days",
   ],
 };
+
+
+async function loadNews() {
+  try{
+    const response = await fetch("http://localhost:8080/api/market/news")
+
+    const data = await response.json();
+
+    const newsList = document.getElementById("news-list");
+
+    //清空newslist
+    newsList.innerHTML = '';
+
+    data.slice(0, 10).forEach((item) => {
+      const newsItem =  `<div class="news-item" style="display:flex; gap:12px; margin-bottom:16px;">
+
+        <img src="${item.image}"
+             style="width:100px; height:70px; object-fit:cover; border-radius:8px;" />
+
+        <div>
+          <a href="${item.url}" target="_blank"
+             style="font-weight:600; text-decoration:none; color:#111;">
+            ${item.headline}
+          </a>
+
+          <p style="font-size:12px; color:gray; margin:4px 0;">
+            ${item.source} · ${new Date(item.datetime * 1000).toLocaleString()}
+          </p>
+
+          <p style="font-size:13px;">
+            ${item.summary}
+          </p>
+        </div>
+      </div>`;
+      newsList.innerHTML += newsItem;
+    });
+  } catch (error) {
+    console.error("加载新闻失败:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadNews)
